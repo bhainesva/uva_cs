b4a17ee9cbf643838b2c8eae7007d0620b733248
bhainesva <bhainesva@gmail.com>
2014-09-01 22:36:15 -0400

    initial commit

---
+++ Lab1/test.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <limits>
+using namespace std;
+
+int xton(int base, int n) {
+    if (!n) {
+	return 1;
+    }
+    if (n == 1){
+	return base;
+    }
+
+   return base * xton(base, n-1);
+}
+
+int main( ) {
+    int x, y, z; 
+    cout << "This program computes x^n for n>0, x and n must be ints" << endl;
+    cout << "Enter x: ";
+    cin >> x;
+    if (cin.fail()){
+	cout << "That wasn't an int, using 1 instead." << endl;
+	cin.clear();
+	cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	x = 1;
+    }
+    cout << "Enter n: ";
+    cin >> y;
+    if (cin.fail()){
+        cout << "That wasn't an int, using 1 instead." << endl;
+	cin.clear();
+	cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        y = 1;
+    } 
+    if (y < 0) {
+      cout << "n must be greater than 0" << endl;
+      return 0;
+    }
+    z = xton(x, y);
+    cout << x << " ^ " << y << " = " << z << endl;
+    return 0;
+}
