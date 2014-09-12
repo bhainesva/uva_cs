c855e3b7bccd0f8149fb01c7707d7fc3300590d0
bmh5wx <bmh5wx@virginia.edu>
2014-09-12 18:18:16 -0400

    initial pre-lab, mostly complete, need to ask questions at office hours

---
+++ Lab3/postfixCalculator.cpp
@@ -0,0 +1,64 @@
+#include <stack>
+#include <iostream>
+#include "postfixCalculator.h"
+
+using namespace std;
+
+PostfixCalculator::PostfixCalculator() {
+    stack<int> s;
+}
+
+PostfixCalculator::~PostfixCalculator() {
+   //Do Nothing; 
+}
+
+bool PostfixCalculator::isEmpty() const {
+    return s.empty();
+}
+
+void PostfixCalculator::pushNum(int num) {
+    s.push(num);
+}
+
+void PostfixCalculator::add() {
+    int x = s.top();
+    s.pop();
+    int y = s.top();
+    s.pop();
+    this->pushNum(x+y);
+}
+
+void PostfixCalculator::subtract() {
+    int x = s.top();
+    s.pop();
+    int y = s.top();
+    s.pop();
+    cout << x-y << endl;
+    this->pushNum(x-y);
+}   
+
+void PostfixCalculator::multiply() {
+    int x = s.top();
+    s.pop();
+    int y = s.top();
+    s.pop();
+    this->pushNum(y*x);
+}
+
+void PostfixCalculator::divide() {
+    int x = s.top();
+    s.pop();
+    int y = s.top();
+    s.pop();
+    this->pushNum(y/x);
+}
+
+void PostfixCalculator::negate() {
+    int x = s.top();
+    s.pop();
+    this->pushNum(-x);
+}
+
+int PostfixCalculator::getTopValue() {
+    return s.top();
+}
